04cdb9b1e058a7550914142086a5046e778ae262
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-04 22:52:32 +0200

    feat(header): Add style

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,35 @@
 import './Header.scss';
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const position = window.scrollY;
+            setScrollPosition(position);
+        };
+
+        window.addEventListener("scroll", handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const headerStyle = {
+        backgroundColor: scrollPosition > 0 ? "#1c2029" : "transparent",
+    };
+
     return (
-        <header>
+        <header style={headerStyle}>
             <div className="container">
                 <h1>Alexandre Vernet</h1>
                 <nav>
                     <ul>
+                        <li><a href="#about">Accueil</a></li>
                         <li><a href="#about">A propos</a></li>
-                        <li><a href="#projects">Projets</a></li>
+                        <li><a href="#projects">Mes projets</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </nav>
