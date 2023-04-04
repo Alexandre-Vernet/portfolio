97fe27c006e3f8219610010604234f5e39af3105
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-04 22:23:42 +0200

    WIP - Design About section

--- src/About/About.js
+++ src/About/About.js
@@ -1,16 +1,39 @@
 import './About.scss';
+import Linkedin from "../images/svg/Linkedin";
+import Github from "../images/svg/Github";
+import CV from "../files/CV.pdf";
+import Email from "../images/svg/Email";
 
 const About = () => {
     return (
         <section id="about">
-            <div className="container">
-                <h2>A propos de moi</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel elit elit. Nullam suscipit
-                    turpis lectus, quis ultricies nisi placerat ut. Donec malesuada libero nec lacus feugiat, in
-                    laoreet nulla scelerisque. </p>
-            </div>
+            <section className="left">
+                <div className="text">
+                    <h2>Hello, it's me</h2>
+                    <h1>Alexandre Vernet</h1>
+                    <h2>I'm a <span className="job-name">full-stack developer</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut odio cursus, finibus lectus
+                        ac
+                    </p>
+                </div>
+                <div className="social-medias">
+                    <Email/>
+                    <Linkedin/>
+                    <Github/>
+                </div>
+                <div className="curriculum-vitae">
+                    <a href={ CV } target="_blank">
+                        <button>Download CV</button>
+                    </a>
+                </div>
+            </section>
+            <section className="right">
+                <img
+                    src="https://user-images.githubusercontent.com/72151831/229601306-fae7a7d3-7d84-4451-b7a4-9241da7e7e3c.png"
+                    alt="photo me"/>
+            </section>
         </section>
-    )
+    );
 }
 
 export default About;
