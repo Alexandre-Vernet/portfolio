87515a4d891b9eeb051ea69bfe9882540e3496c0
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-04 22:24:24 +0200

    fix: Add property in href

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
             <div className="container">
                 <h2>Mes projets</h2>
                 <div className="project-list">
-                    <a href="" target="_blank">
+                    <a href="#" target="_blank">
                         <div className="project">
                             <img
                                 src="https://user-images.githubusercontent.com/72151831/229601306-fae7a7d3-7d84-4451-b7a4-9241da7e7e3c.png"
@@ -15,7 +15,7 @@ const Projects = () => {
                             <p>Synchronisation de fichiers d'un appareil à un autre</p>
                         </div>
                     </a>
-                    <a href="" target="_blank">
+                    <a href="#" target="_blank">
                         <div className="project">
                             <img
                                 src="https://user-images.githubusercontent.com/72151831/229601306-fae7a7d3-7d84-4451-b7a4-9241da7e7e3c.png"
@@ -24,7 +24,7 @@ const Projects = () => {
                             <p>Synchronisation de fichiers d'un appareil à un autre</p>
                         </div>
                     </a>
-                    <a href="" target="_blank">
+                    <a href="#" target="_blank">
                         <div className="project">
                             <img
                                 src="https://user-images.githubusercontent.com/72151831/229601306-fae7a7d3-7d84-4451-b7a4-9241da7e7e3c.png"
