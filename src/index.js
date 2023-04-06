ccf901acf373fee663b6b382d688bf34b8799630
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-06 20:35:34 +0200

    feat(language): Add projects section

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
-import common_fr from "./translations/fr/common.json";
-import common_en from "./translations/en/common.json";
+import common_fr from "./translations/fr.json";
+import common_en from "./translations/en.json";
 
 i18next.init({
     interpolation: { escapeValue: false },  // React already does escaping
-    lng: 'fr',                              // language to use
+    lng: 'en',                              // language to use
     resources: {
         en: {
             common: common_en               // 'common' is our custom namespace
