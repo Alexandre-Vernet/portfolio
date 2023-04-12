9593b24425fdd6750f3df0290def706748335a28
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-12 17:54:03 +0200

    feat(contact): Add contact section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Projects from "./components/Projects/Projects";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Header/>
             <About/>
             <Projects/>
+            <Contact />
         </div>
     );
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,30 @@
 import './Contact.scss';
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+    const { t } = useTranslation('common');
+
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    )
+        <section className="contact" id="contact">
+            <div className="container">
+                <h2>{ t('contact.contact') } <span>{ t('contact.me') }</span></h2>
+                <div className="contact-container">
+                    <form action="">
+                        <div className="form-group">
+                            <input type="text" required placeholder={ t('contact.full_name') }/>
+                            <input type="email" required placeholder={ t('contact.email') }/>
+                        </div>
+                        <div className="form-group">
+                            <textarea cols="30" rows="10" required placeholder={ t('contact.email_description') }></textarea>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit">{ t('contact.send') }</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Contact;
