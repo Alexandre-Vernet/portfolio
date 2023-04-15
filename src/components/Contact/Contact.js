8d9b566177519a69785636a06164e81af9e811b5
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-15 19:30:02 +0200

    feat(contact email): Send email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,24 +1,52 @@
 import './Contact.scss';
 import { useTranslation } from "react-i18next";
+import axios from "axios";
+import { useState } from "react";
 
 const Contact = () => {
-    const { t } = useTranslation('common');
+    const {t} = useTranslation('common');
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleInputChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value});
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        const endpoint = 'http://localhost:3333';
+
+        axios.post(endpoint, formData)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 
     return (
         <section className="contact" id="contact">
             <div className="container">
                 <h2>{ t('contact.contact') } <span>{ t('contact.me') }</span></h2>
                 <div className="contact-container">
-                    <form action="">
+                    <form>
                         <div className="form-group">
-                            <input type="text" required placeholder={ t('contact.full_name') }/>
-                            <input type="email" required placeholder={ t('contact.email') }/>
+                            <input type="text" value={ formData.name } onChange={ handleInputChange } required
+                                   placeholder={ t('contact.full_name') }/>
+                            <input type="email" value={ formData.email } onChange={ handleInputChange } required
+                                   placeholder={ t('contact.email') }/>
                         </div>
                         <div className="form-group">
-                            <textarea cols="30" rows="10" required placeholder={ t('contact.email_description') }></textarea>
+                            <textarea cols="30" rows="10" required value={ formData.message }
+                                      onChange={ handleInputChange }
+                                      placeholder={ t('contact.email_description') }></textarea>
                         </div>
                         <div className="form-group">
-                            <button type="submit">{ t('contact.send') }</button>
+                            <button onClick={ (e) => sendEmail(e) } type="button">{ t('contact.send') }</button>
                         </div>
                     </form>
                 </div>
