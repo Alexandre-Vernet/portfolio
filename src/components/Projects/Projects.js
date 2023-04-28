74d971974190e6c313c374f8f04d516a36e70a7b
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-28 18:08:16 +0200

    feat(projects): Add new design

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,33 +1,22 @@
 import './Projects.scss';
 import { useTranslation } from "react-i18next";
-import project1 from '../../images/jpg/project-1.jpg';
-import project2 from '../../images/jpg/project-2.jpg';
-import project3 from '../../images/jpg/project-3.jpg';
-import project4 from '../../images/jpg/project-4.jpg';
-import project5 from '../../images/jpg/project-5.jpg';
-import project6 from '../../images/jpg/project-6.jpg';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Projects = () => {
     const { t } = useTranslation('common');
-    const projectsImages = [project1, project2, project3, project4, project5, project6];
+    const projects = Array.from(t('projects.my_projects', { returnObjects: true }));
 
     return (<section className="projects" id="projects">
         <div className="section-container">
             <h2>{ t('projects.my_latest') } <span>{ t('projects.projects') }</span></h2>
             <div className="project-container">
-                { projectsImages.map((project, index) => (
+                { projects.map((project, index) => (
                     <div className="project-box"
                          key={ index }>
-                        <a href={ t(`projects.my_projects.${ index }.url`) } target="_blank" rel="noreferrer">
-                            <LazyLoadImage
-                                alt={ project + index }
-                                src={ project }
-                                effect="blur"
-                            />
+                        <a className="project" href={ t(`projects.my_projects.${ index }.url`) } target="_blank" rel="noreferrer">
+                            <img src={ project.image } alt=""/>
                             <div className="project-layer">
-                                <h4>{ t(`projects.my_projects.${ index }.name`) }</h4>
-                                <p>{ t(`projects.my_projects.${ index }.description`) }</p>
+                                <h4>{ project.name }</h4>
+                                <p>{ project.description }</p>
                             </div>
                         </a>
                     </div>)) }
